651ca1ebf8acc5212c19e671f5a238f5502faf9a
JAEYOON <log901108@gmail.com>
2018-01-27 14:55:52 +0900

    포켓몬용 database 수정

--- HeptaAPI/database.cpp
+++ HeptaAPI/database.cpp
@@ -27,35 +27,35 @@ void database::release()
 
 void database::loadDatabase(string name)
 {
-	//arrElements vTemp;
-	//vTemp = TXTDATA->txtLoad(name.c_str());
-	//
-	//string str;
-	//int count = 0;
-	//
-	//for (int i = 0; i < vTemp.size(); ++i)
-	//{
-	//	//칸막이를 찾았으면
-	//	if (vTemp[i] == "|")
-	//	{
-	//		elements* em = new elements;
-	//		str = vTemp[i + 1];
-	//
-	//		_mTotalElement.insert(pair<string, elements*>(vTemp[i + 1], em));
-	//
-	//		if (i != 0) count += 7;
-	//		continue;
-	//	}
-	//
-	//	iterElement mIter = _mTotalElement.find(str);
-	//
-	//	if (i == count + 1) mIter->second->name = vTemp[i].c_str();
-	//	else if (i == count + 2) mIter->second->currentHP = atoi(vTemp[i].c_str());
-	//	else if (i == count + 3) mIter->second->maxHP = atoi(vTemp[i].c_str());
-	//	else if (i == count + 4) mIter->second->angle = (float)atof(vTemp[i].c_str());
-	//	else if (i == count + 5) mIter->second->maxSpeed = (float)atof(vTemp[i].c_str());
-	//	else if (i == count + 6) mIter->second->accelaration = (float)atof(vTemp[i].c_str());
-	//}
-	//
-	//vTemp.clear();
+	arrElements vTemp;
+	vTemp = TXTDATA->txtLoad(name.c_str());
+	
+	string str;
+	int count = 0;
+	
+	for (int i = 0; i < vTemp.size(); ++i)
+	{
+		//칸막이를 찾았으면
+		if (vTemp[i] == "|")
+		{
+			elements* em = new elements;
+			str = vTemp[i + 1];
+	
+			_mTotalElement.insert(pair<string, elements*>(vTemp[i + 1], em));
+	
+			if (i != 0) count += 7;
+			continue;
+		}
+	
+		iterElement mIter = _mTotalElement.find(str);
+	
+		if (i == count + 1) mIter->second->name = vTemp[i].c_str();
+		else if (i == count + 2) mIter->second->hp = atoi(vTemp[i].c_str());
+		else if (i == count + 3) mIter->second->atk = atoi(vTemp[i].c_str());
+		else if (i == count + 4) mIter->second->def = atoi(vTemp[i].c_str());
+		else if (i == count + 5) mIter->second->speed = atoi(vTemp[i].c_str());
+		else if (i == count + 6) mIter->second->special = atoi(vTemp[i].c_str());
+	}
+	
+	vTemp.clear();
 }
--- HeptaAPI/database.h
+++ HeptaAPI/database.h
@@ -5,6 +5,20 @@
 
 class pokemon;
 
+class elements
+{
+public:
+	string name;
+	int hp;
+	int atk;
+	int def;
+	int speed;
+	int special;
+
+	elements() {};
+	~elements() {};
+};
+
 //내부 싱글톤 DB 만들어둔다
 class database : public singletonBase <database>
 {
@@ -12,15 +26,27 @@ private:
 	typedef vector<pokemon*> vPokemon;
 	typedef vector<pokemon*>::iterator viPokemon;
 
+	typedef vector<string> arrElements;
+	typedef vector<string>::iterator iterElements;
+
+	typedef map<string, elements*> arrElement;
+	typedef map<string, elements*>::iterator iterElement;
+
 private:
 	vPokemon* _vPlayerPokemon;
 	vPokemon* _vEnemyPokemon;
+
+	// 포켓몬 종족값 등록한 맵
+	arrElement _mTotalElement;
 
 public:
 	HRESULT init();
 	void release();
 
 	void loadDatabase(string name);
+
+	//속성에 대한 접근자
+	elements* getElementData(string str) { return _mTotalElement.find(str)->second; }
 
 	database();
 	~database();
